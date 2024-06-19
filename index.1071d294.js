48c087df1a19613e3af3020c44dd7dfe2fe0d965
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2024-06-19 15:14:20 +0300

    make build

---
+++ index.1071d294.js
@@ -0,0 +1,2 @@
+window.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".wall"),t=document.querySelector(".spider"),n=e.getBoundingClientRect(),o=t.getBoundingClientRect(),d=(n.width-o.width)/2,i=(n.height-o.height)/2;t.style.left="".concat(d-10,"px"),t.style.top="".concat(i-10,"px")});
+//# sourceMappingURL=index.1071d294.js.map
